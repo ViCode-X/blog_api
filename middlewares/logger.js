2b2b538b434e28e68b8f983d35243fbf9419e2a7
ViCode-X <adebiyivictor5555@gmail.com>
2026-08-15 10:31:30 +0000

    improved logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,12 @@
 const logRequest = (req, res, next) => {
     const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${req.method}- ${req.url} from ${req.ip}`);
+
+    res.on('finish', () => {
+        console.log(
+            `[${timestamp}] ${req.method} - ${req.url} ${res.statusCode} from ${req.ip}`
+        );
+    });
+
     next();
 };
 
