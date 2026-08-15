a5d0ed5b291a9d53a6539ec7c407ffc7924bec66
ViCode-X <adebiyivictor5555@gmail.com>
2026-08-15 17:45:21 +0000

    additional search indexes

--- models/article.model.js
+++ models/article.model.js
@@ -71,6 +71,9 @@ articleSchema.index({
     content: "text",
     excerpt: "text",
     tags: "text",
+    category :"text",
+    author: "text",
+    slug: "text"
 });
 
 // Automatically generate slug from title
